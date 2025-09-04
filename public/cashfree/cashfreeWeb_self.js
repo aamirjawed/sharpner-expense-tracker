5dffb9d3a7888a93fab8af7d70548161e24c8bdb
aamirjawed <aamirjawed682@gmail.com>
2025-09-04 16:30:05 +0530

    little

--- public/cashfree/cashfreeWeb_self.js
+++ public/cashfree/cashfreeWeb_self.js
@@ -5,7 +5,7 @@ const cashfree = Cashfree({
 document.getElementById("renderBtn").addEventListener("click", async () => {
   try {
     // Fetch payment session ID from backend
-    const response = await fetch("https://expense-tracker-q5t0.onrender.com/payment-status/payment/pay", {
+    const response = await fetch("https://expense-tracker-q5t0.onrender.com/payment/pay", {
       method: "POST",
     });
 
